57864e478905bd89777cc94e04bdc1e84d9cb689
mystic_alien <hellemony@gmail.com>
2023-09-15 10:41:41 +0300

    Commit message

--- src/pac1/ArraySumAverage.java
+++ src/pac1/ArraySumAverage.java
@@ -1,2 +1,26 @@
-package pac1;public class ArraySumAverage {
+package pac1;
+import java.util.Scanner;
+
+public class ArraySumAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // System.in представляет стандартный входной поток в Java
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] numbers = new int[size]; // new int[size] - это операция создания нового массива
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Введите число: ");
+            numbers[i] = scanner.nextInt();
+            sum += numbers[i];
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Сумма элементов: " + sum);
+        System.out.println("Среднее арифметическое: " + average);
+    }
 }
+
--- src/pac1/HarmonicSeries.java
+++ src/pac1/HarmonicSeries.java
@@ -1,2 +1,10 @@
-package pac1;public class HarmonicSeries {
+package pac1;
+
+public class HarmonicSeries {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            double harmonic = 1.0 / i;
+            System.out.printf("Число %d: %.4f%n", i, harmonic);
+        }
+    }
 }
